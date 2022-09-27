d8fba3cd8d17de5474eb8b66d005317208bac1b6
1728858836 <mag.tandalla@yavirac.edu.ec>
2022-09-27 18:02:48 -0500

    Social your feed

--- src/app/feature/falcon/dasboard/social-add-to-your-feed/social-add-to-your-feed.component.ts
+++ src/app/feature/falcon/dasboard/social-add-to-your-feed/social-add-to-your-feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodeService } from 'src/app/feature/code.service';
 
 @Component({
   selector: 'app-social-add-to-your-feed',
@@ -6,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialAddToYourFeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private codeService: CodeService) { }
 
   ngOnInit(): void {
+    this.codeService.sendCode("https://raw.githubusercontent.com/sistemaagil/tailwindcss/falcon/ecommerce-Total-Sales/src/app/feature/falcon/dasboard/social-add-to-your-feed/social-add-to-your-feed.component.html?token=GHSAT0AAAAAABZH2SPD2QXENPJZ5654F4MMYZTQEQQ")
   }
 
 }
