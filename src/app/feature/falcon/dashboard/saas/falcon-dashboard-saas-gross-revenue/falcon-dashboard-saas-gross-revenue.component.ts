585b8a957800b0f18bf8d67dea6530dcd7763e26
ebq.gualpa@yavirac.edu.ec <ebq.gualpa@yavirac.edu.ec>
2022-09-26 23:15:20 -0500

    nuevo menu Gross Revenue

---
+++ src/app/feature/falcon/dashboard/saas/falcon-dashboard-saas-gross-revenue/falcon-dashboard-saas-gross-revenue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-falcon-dashboard-saas-gross-revenue',
+  templateUrl: './falcon-dashboard-saas-gross-revenue.component.html',
+  styleUrls: ['./falcon-dashboard-saas-gross-revenue.component.css']
+})
+export class FalconDashboardSaasGrossRevenueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -72,6 +72,7 @@ import { FalconDashboardManagementTODOLISTComponent } from './falcon/dashboard/f
 import { FalconDashboardManagementWELCOMETOFALCONComponent } from './falcon/dashboard/falcon-dashboard-management-welcome-tofalcon/falcon-dashboard-management-welcome-tofalcon.component';
 import { FalconDashboardDefaultTOPPRODUCTSComponent } from './falcon/dashboard/falcon-dashboard-default-top-products/falcon-dashboard-default-top-products.component';
 import { ECommerceSalesByPosLocationComponent } from './falcon/dasboard/e-commerce-sales-by-pos-location/e-commerce-sales-by-pos-location.component';
+import { FalconDashboardSaasGrossRevenueComponent } from './falcon/dashboard/saas/falcon-dashboard-saas-gross-revenue/falcon-dashboard-saas-gross-revenue.component';
 
 
 @NgModule({
@@ -151,6 +152,7 @@ import { ECommerceSalesByPosLocationComponent } from './falcon/dasboard/e-commer
     FalconDashboardManagementWELCOMETOFALCONComponent,
     FalconDashboardDefaultTOPPRODUCTSComponent,
     ECommerceSalesByPosLocationComponent,
+    FalconDashboardSaasGrossRevenueComponent,
   ],
   imports: [
     CommonModule,
