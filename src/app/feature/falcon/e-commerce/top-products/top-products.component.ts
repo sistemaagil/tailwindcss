0753d019f3963b14dcc48fafd8916b46fc1aca19
JohanP151 <jas.pavon@yavirac.edu.ec>
2022-09-28 11:03:34 -0500

    rama terminada top products

--- src/app/feature/falcon/e-commerce/top-products/top-products.component.ts
+++ src/app/feature/falcon/e-commerce/top-products/top-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-top-products',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-products.component.css']
 })
 export class TopProductsComponent implements OnInit {
+  title = 'ng2-charts-demo';
+
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ 'Boots4', '', 'Slick', '', 'Sparrow', '', 'Freya' ],
+    datasets: [
+      { data: [ 43, 83, 86, 72, 80, 50,80 ], label: '2019 ' ,backgroundColor: 'rgba(59 130 246)'},
+      { data: [ 85, 73, 62, 53, 50, 70, 90 ], label: '2018 ' , backgroundColor: 'rgba(203 213 225)'},
+
+
+    ]
+  };
 
   constructor() { }
 
