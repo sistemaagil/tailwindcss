ad37121ed5df3b50a63c76286418698b3ebbdfe9
1752378099 <ead.guevara@yavirac.edu.ec>
2022-09-28 13:35:37 -0500

    Agregar calendar

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -72,6 +72,7 @@ import { FalconDashboardManagementTODOLISTComponent } from './falcon/dashboard/f
 import { FalconDashboardManagementWELCOMETOFALCONComponent } from './falcon/dashboard/falcon-dashboard-management-welcome-tofalcon/falcon-dashboard-management-welcome-tofalcon.component';
 import { FalconDashboardDefaultTOPPRODUCTSComponent } from './falcon/dashboard/falcon-dashboard-default-top-products/falcon-dashboard-default-top-products.component';
 import { ECommerceSalesByPosLocationComponent } from './falcon/dasboard/e-commerce-sales-by-pos-location/e-commerce-sales-by-pos-location.component';
+import { CalendarComponent } from './falcon/calendar/calendar.component';
 
 
 @NgModule({
@@ -151,6 +152,7 @@ import { ECommerceSalesByPosLocationComponent } from './falcon/dasboard/e-commer
     FalconDashboardManagementWELCOMETOFALCONComponent,
     FalconDashboardDefaultTOPPRODUCTSComponent,
     ECommerceSalesByPosLocationComponent,
+    CalendarComponent,
   ],
   imports: [
     CommonModule,
