b77eb4bca5b9659a64258faa96f6ef7982266380
JohanP151 <jas.pavon@yavirac.edu.ec>
2022-09-29 18:41:45 -0500

    rama finalizada

---
+++ src/app/feature/falcon/eemail/falcon-app-eemail-composee/falcon-app-eemail-composee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-falcon-app-eemail-composee',
+  templateUrl: './falcon-app-eemail-composee.component.html',
+  styleUrls: ['./falcon-app-eemail-composee.component.css']
+})
+export class FalconAPPEemailComposeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -88,6 +88,7 @@ import { CustomersComponent } from './falcon/management/customers/customers.comp
 import { SaasRevenueComponent } from './falcon/dasboard/saas-revenue/saas-revenue.component';
 import { SaasConversionComponent } from './falcon/dasboard/saas-conversion/saas-conversion.component';
 import { FalconAPPEemailEemailDetailComponent } from './falcon/eemail/falcon-app-eemail-eemail-detail/falcon-app-eemail-eemail-detail.component';
+import { FalconAPPEemailComposeeComponent } from './falcon/eemail/falcon-app-eemail-composee/falcon-app-eemail-composee.component';
 
 
 @NgModule({
@@ -180,6 +181,7 @@ import { FalconAPPEemailEemailDetailComponent } from './falcon/eemail/falcon-app
     SaasActiveUsersComponent,
     FalconDashboardSaasMOREWITHFALCONComponent,
     FalconAPPEemailEemailDetailComponent,
+    FalconAPPEemailComposeeComponent,
   ],
   imports: [
     CommonModule,
