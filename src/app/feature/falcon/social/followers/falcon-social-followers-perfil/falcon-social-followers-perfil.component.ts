1d7b34425d7a16467ea2cda30e971a7c72095e6f
TailwindccsSA <tailwindcsssa@gmail.com>
2022-09-29 19:46:18 -0500

    agrega perfil

--- src/app/feature/falcon/social/followers/falcon-social-followers-perfil/falcon-social-followers-perfil.component.ts
+++ src/app/feature/falcon/social/followers/falcon-social-followers-perfil/falcon-social-followers-perfil.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-falcon-social-followers-perfil',
-  templateUrl: './falcon-social-followers-perfil.component.html',
-  styleUrls: ['./falcon-social-followers-perfil.component.css']
+  templateUrl: './falcon-social-followers-perfil.component.html'
 })
 export class FalconSocialFollowersPerfilComponent implements OnInit {
 
