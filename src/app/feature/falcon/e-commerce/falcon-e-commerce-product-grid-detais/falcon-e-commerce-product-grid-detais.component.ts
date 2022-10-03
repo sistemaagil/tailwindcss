6bf5e21a07402620784a58d6b86bdce1b0561147
aji.torres <aji.torres@yavirac.edu.ec>
2022-10-03 18:50:15 -0500

    htlm de product grid

--- src/app/feature/falcon/e-commerce/falcon-e-commerce-product-grid-detais/falcon-e-commerce-product-grid-detais.component.ts
+++ src/app/feature/falcon/e-commerce/falcon-e-commerce-product-grid-detais/falcon-e-commerce-product-grid-detais.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodeService } from 'src/app/feature/code.service';
 
 @Component({
   selector: 'app-falcon-e-commerce-product-grid-detais',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FalconECommerceProductGridDetaisComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private codeService: CodeService) { }
   ngOnInit(): void {
+    this.codeService.sendCode("https://raw.githubusercontent.com/sistemaagil/tailwindcss/feature/falcon/e-commerce/product-grid-details/src/app/feature/falcon/e-commerce/falcon-e-commerce-product-grid-detais/falcon-e-commerce-product-grid-detais.component.html");
   }
 
 }
