3abdd6e47769d4bfb1eefac8ccf70273d6da3e90
ebq.gualpa@yavirac.edu.ec <ebq.gualpa@yavirac.edu.ec>
2022-10-03 04:10:11 -0500

    agregando link

--- src/app/feature/falcon/dashboard/saas/falcon-dashboard-saas-bandwidth-saved/falcon-dashboard-saas-bandwidth-saved.component.ts
+++ src/app/feature/falcon/dashboard/saas/falcon-dashboard-saas-bandwidth-saved/falcon-dashboard-saas-bandwidth-saved.component.ts
@@ -11,7 +11,7 @@ export class FalconDashboardSaasBANDWIDTHSAVEDComponent implements OnInit {
   constructor(private codeService: CodeService) { }
 
   ngOnInit(): void {
-    this.codeService.sendCode("https://raw.githubusercontent.com/sistemaagil/tailwindcss/develop/src/app/feature/falcon/dashboard/saas/falcon-dashboard-saas-bandwidth-saved/falcon-dashboard-saas-bandwidth-saved.component.html?token=GHSAT0AAAAAABZHUGZ6XB6AUSADSITWZQ3GYZ2ULNA");
+    this.codeService.sendCode("https://raw.githubusercontent.com/sistemaagil/tailwindcss/feature/falc%C3%B3n/saas/BANDWIDTH_SAVED/src/app/feature/falcon/dashboard/saas/falcon-dashboard-saas-bandwidth-saved/falcon-dashboard-saas-bandwidth-saved.component.html?token=GHSAT0AAAAAABZHUGZ6CERCFXMQQONGJI54YZ2U2NA");
   }
 
 }
