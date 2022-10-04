0bcbff9b39df0b50b5fb7fa46188e4036b9aa0a0
aji.torres <aji.torres@yavirac.edu.ec>
2022-10-04 17:35:52 -0500

    product details 6 linkiado

--- src/app/feature/falcon/e-commerce/falcon-product-details-seis/falcon-product-details-seis.component.ts
+++ src/app/feature/falcon/e-commerce/falcon-product-details-seis/falcon-product-details-seis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodeService } from 'src/app/feature/code.service';
 
 @Component({
   selector: 'app-falcon-product-details-seis',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FalconProductDetailsSeisComponent implements OnInit {
 
-  constructor() { }
+  constructor(private codeService: CodeService) { }
 
   ngOnInit(): void {
+    this.codeService.sendCode("https://github.com/sistemaagil/tailwindcss/blob/falcon/e-commerce/productDetails_6/src/app/feature/falcon/e-commerce/falcon-product-details-seis/falcon-product-details-seis.component.html");
   }
 
 }
