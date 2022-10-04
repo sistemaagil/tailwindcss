3563893218e753cf9b5878f41c25866350ad48c2
Daniel <danielvera614@gmail.com>
2022-10-04 18:14:27 -0500

    arregla boton shared files

--- src/app/feature/falcon/default/shared-files/shared-files.component.ts
+++ src/app/feature/falcon/default/shared-files/shared-files.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodeService } from 'src/app/feature/code.service';
 
 @Component({
   selector: 'app-shared-files',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedFilesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private codeService: CodeService) { }
 
   ngOnInit(): void {
+    
+    this.codeService.sendCode("https://raw.githubusercontent.com/sistemaagil/tailwindcss/falcon/events/texto-new-year/src/app/feature/falcon/default/shared-files/shared-files.component.html");
   }
 
 }
